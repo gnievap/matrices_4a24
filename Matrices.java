1466b9fac2b2c47cb308ad849243bd080f7cf578
Gaby Nieva <gaby.nieva@outlook.com>
2024-10-16 12:00:35 -0600

    Refactorizando métodos de matriz irregular

--- Matrices.java
+++ Matrices.java
@@ -18,13 +18,37 @@ public class Matrices {
             }
             System.out.println();
         }
+    }
+
+    public static int[][] crearMatrizIrregular(int r) throws IOException{
+        int c;
+        int[][] matriz2 = new int[r][];
+        for ( int i = 0; i < matriz2.length; i++ ){
+            System.out.println("Escriba columnas para el renglón " + i + ": ");
+            entrada = bufer.readLine();
+            c = Integer.parseInt(entrada);
+            matriz2[i] = new int[c];
+        }
+        return matriz2;
+    }
+
+    public static int[][] llenarMatrizIrregular(int[][] m) throws IOException{
+        System.out.println("Introduciendo valores en una matriz irregular");
+        for ( int i = 0; i < m.length; i++ ){
+            for ( int j = 0; j < m[i].length; j++) {
+                System.out.println("Escriba el valor de ["+i+"]["+j+"]:");
+                entrada = bufer.readLine();
+                m[i][j] = Integer.parseInt(entrada);
+            }
+        }
+        return m;
 
     }
 
     public static void main(String[] args) throws IOException{
         int[][] matriz1 = new int[3][3];
         int numero = 1;
-        int renglones, columnas;
+        int renglones;
 
         for (int i = 0; i < matriz1.length; i++ ){
             for ( int j = 0; j < matriz1[i].length; j++ ){
@@ -41,18 +65,9 @@ public class Matrices {
         entrada = bufer.readLine();
         renglones = Integer.parseInt(entrada);
         // Construyendo la matriz2
-        matriz2 = new int[renglones][];
-        for ( int i = 0; i < matriz2.length; i++ ){
-            System.out.println("Escriba columnas para el renglón " + i + ": ");
-            entrada = bufer.readLine();
-            columnas = Integer.parseInt(entrada);
-            matriz2[i] = new int[columnas];
-            numero = 1;
-            for ( int j = 0; j < matriz2[i].length; j++ ){
-                matriz2[i][j] = numero;
-                numero++;
-            } 
-        }
+        matriz2 = crearMatrizIrregular(renglones);
+        // Colocar valores en matriz2
+        matriz2 = llenarMatrizIrregular(matriz2);
         // imprimiendo la matriz2
         System.out.println("Valores de la matriz irregular: ");
         imprimirMatriz(matriz2);
